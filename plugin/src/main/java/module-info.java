0a907d562de5108eaa4c66bce04f41ddd20d6a71
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-17 10:39:24 +0300

    drop ide.api deps

--- plugin/src/main/java/module-info.java
+++ plugin/src/main/java/module-info.java
@@ -3,7 +3,13 @@
  * @since 2026-01-31
  */
 module consulo.serial.monitor {
-    requires consulo.ide.api;
+    requires consulo.language.editor.api;
+    requires consulo.execution.api;
+    requires consulo.execution.debug.api;
+    requires consulo.file.chooser.api;
+    requires consulo.ui.ex.api;
+    requires consulo.project.ui.api;
+    requires consulo.process.api;
 
     requires com.fazecast.jSerialComm;
     requires jediterm.core;
